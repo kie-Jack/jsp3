2f5711c11a6b380e1ca9e3bffe90151cc90e851a
HwangYoonChan <jackie3923@hotmail.com>
2021-10-05 15:13:41 +0900

    Day 11 - HttpServlet

---
+++ WebContent/index.jsp
@@ -0,0 +1,14 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>서블릿 INDEX</title>
+</head>
+<body>	<!-- a태그 href 는 요청메소드 GET 입니다. -->
+<a href="first.dev">첫번째 서블릿</a>	<br>
+<a href="second.dev">두번째 서블릿</a> <br>
+<a href="3th.dev">세번째 서블릿</a>
+</body>
+</html>
--- src/com/jcpdev/action/FirstServlet.java
+++ src/com/jcpdev/action/FirstServlet.java
@@ -3,13 +3,15 @@ package com.jcpdev.action;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 // 서블릿은 http 요청을 처리하는 자바 클래스입니다. HttpServlet을 상속받습니다.
-public class Login extends HttpServlet{
+public class FirstServlet extends HttpServlet{
 	
 	private static final long serialVersionUID = 1L;
 	
@@ -22,9 +24,23 @@ public class Login extends HttpServlet{
 		response.setCharacterEncoding("UTF-8");
 		response.setContentType("text/html");
 		PrintWriter out = response.getWriter();		//JspWriter의 부모 PrintWriter 타입으로 선언. 위의 2개 response set 필요
-		
+		//응답할 html 문서의 내용
 		out.print("<h3>JSP와 서블릿</h3>");
 		out.print("Who the fuck are you, Servlet?");
+		
+		//현재 request 객체에 대한 세션객체가 필요하다면
+		HttpSession session = request.getSession();
+		session.setAttribute("data", "first");
+		
+		//1) request 에 애트리뷰트 객체 저장 (선택)
+		request.setAttribute("number", 123);
+		//**pageContext.forward("first.jsp");		-> pageContext는 JSP파일에서만 사용할 수 있는 내장객체.
+		//서블릿에서는 다음 객체를 사용합니다.
+		//**first.jsp(view 파일)가 현재의 request, response 전달받을 수 있도록 감싸는 객체가 RequestDispatcher
+		//2) view 파일로 forward
+		RequestDispatcher rd = request.getRequestDispatcher("first.jsp");
+		rd.forward(request, response);
+		
 	}
 	
 	//요청 Method가 POST 일 때 처리되는 메소드 
